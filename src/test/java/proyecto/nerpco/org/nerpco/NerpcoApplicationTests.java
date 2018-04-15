ef51d70e0ac96a546f5da6a2254f9d5915f7d7c8
luisabrahamaquinocastillo <luis.aquino.135769@gmail.com>
2018-04-15 18:25:53 -0500

    Nerpco V-2.0

---
+++ src/test/java/proyecto/nerpco/org/nerpco/NerpcoApplicationTests.java
@@ -0,0 +1,16 @@
+package proyecto.nerpco.org.nerpco;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NerpcoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
