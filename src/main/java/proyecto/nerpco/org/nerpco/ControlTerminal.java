ba816477cb21839acd79bc5abd3f59a353a97547
luisabrahamaquinocastillo <luis.aquino.135769@gmail.com>
2018-04-17 18:13:54 -0500

    Nerpco Version 2.3 Actualización 6:13pm

--- src/main/java/proyecto/nerpco/org/nerpco/ControlTerminal.java
+++ src/main/java/proyecto/nerpco/org/nerpco/ControlTerminal.java
@@ -2,7 +2,11 @@ package proyecto.nerpco.org.nerpco;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.ReactiveInsertOperation;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin
@@ -23,6 +27,7 @@ public class ControlTerminal {
         return new Estatus(true,"Terminal guardada con exito");
     }
 
+    //Metodo JSON para actualizar terminal
     @CrossOrigin
     @RequestMapping(value = {"/actualizar"}, method = RequestMethod.PUT, headers = {"Accept=application/json"})
     public Estatus actualizarTerminal(@RequestBody String json)throws Exception{
@@ -34,5 +39,35 @@ public class ControlTerminal {
 
         repoTerm.save(terminales);
         return new Estatus(true,"Terminal actualizada");
+    }
+
+    //Metodo json para borrar terminal
+    @CrossOrigin
+    @RequestMapping(value = {"/borrar"}, method = RequestMethod.DELETE, headers = {"Accept=application/json"})
+    public Estatus borrarTerminal(@RequestBody String json)throws Exception{
+
+        ObjectMapper mapper=new ObjectMapper();
+        Terminales terminales=mapper.readValue(json,Terminales.class);
+
+        repoTerm.delete(terminales);
+        return new Estatus(true, "Terminal eliminada");
+    }
+
+    //Metodo JSON para buscar por ID
+    @CrossOrigin
+    @RequestMapping(value = {"/id"}, method = RequestMethod.GET, headers = {"Accept=application/json"})
+    public Optional<Terminales> buscarTerminal(@RequestBody String json)throws Exception{
+
+        ObjectMapper mapper=new ObjectMapper();
+        Terminales terminales=mapper.readValue(json,Terminales.class);
+
+        return repoTerm.findById(terminales.getClaveTerminal());
+    }
+
+    //buscar todos
+    @CrossOrigin
+    @RequestMapping(value = {"/todos"}, method = RequestMethod.GET, headers = {"Accept=application/json"})
+    public ArrayList<Terminales> buscartodos(){
+        return (ArrayList<Terminales>)repoTerm.findAll();
     }
 }
